c6961902baa5194292de327d0a3cdba38004cc95
james <james@gmail.com>
2017-06-09 15:16:13 +0800

    a

---
+++ Bugatti/Category/UIImage+Nikka.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Nikka.h
+//  Bugatti
+//
+//  Created by toby on 09/06/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Nikka)
+
+-(UIImage*)cutImageWithtargetSize: (CGSize)targetSize;
+
+@end
